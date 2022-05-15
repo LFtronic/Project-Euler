b4cf27402ea460303517ce1633f19bbd6dcb25ec
LFtronic <a01023675@itesm.mx>
2022-05-14 23:02:46 -0500

    began 5

--- JAVA/Euler0005.java
+++ JAVA/Euler0005.java
@@ -4,4 +4,18 @@ import java.util.*;
 
 public class Euler0005 {
     
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int smallNum = n;
+            for (int i = n-1; i > 1; i--) {
+                smallNum *= i;
+            }
+            System.out.println(smallNum);
+        }
+        in.close();
+    }
+
 }
