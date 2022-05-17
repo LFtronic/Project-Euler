6acab2db0877def9fd2151b6f0bf55036b1b06fd
LFtronic <a01023675@itesm.mx>
2022-05-17 04:01:26 -0500

    quick fix to problem 01

--- JAVA/Euler0001.java
+++ JAVA/Euler0001.java
@@ -4,10 +4,10 @@ import java.util.*;
 
 public class Euler0001 {
 
-    public static long getMultiplesOf3and5(long n) {
-        long t1 = 3 * ((n / 3) * ((n / 3) + 1)) / 2;
-        long t2 = 5 * ((n / 5) * ((n / 5) + 1)) / 2;
-        long t3 = 15 * ((n / 15) * ((n / 15) + 1)) / 2;
+    public static long getMultiplesOf(int a, int b, long n) {
+        long t1 = a * ((n / a) * ((n / a) + 1)) / 2;
+        long t2 = b * ((n / b) * ((n / b) + 1)) / 2;
+        long t3 = a*b * ((n / a*b) * ((n / a*b) + 1)) / 2;
         return t1+t2-t3;
     }
 
@@ -16,7 +16,9 @@ public class Euler0001 {
         int t = in.nextInt();
         for(int a0 = 0; a0 < t; a0++){
             long n = in.nextInt();
-            System.out.println(getMultiplesOf3and5(n-1));  
+            int a = 3;
+            int b = 5;
+            System.out.println(getMultiplesOf(a, b, n-1));  
         }
         in.close();
     }
