ffbf369380d2bed9010afa6f157025b6a2690387
LFtronic <a01023675@itesm.mx>
2022-06-09 07:54:27 -0500

    m2

--- Euler0006.java
+++ Euler0006.java
@@ -1,3 +1,11 @@
 public class Euler0006 {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            long n = in.nextInt();
+             
+        }
+        in.close();
+    }
 }
