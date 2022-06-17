835cbdc234dfb3a812a80e0f3ef8459213261f09
LFtronic <a01023675@itesm.mx>
2022-06-17 07:54:27 -0500

    s

--- JAVA/Euler0006.java
+++ JAVA/Euler0006.java
@@ -8,7 +8,7 @@ public class Euler0006 {
         int t = in.nextInt();
         for(int a0 = 0; a0 < t; a0++){
             long n = in.nextInt();
-             
+            
         }
         in.close();
     }
